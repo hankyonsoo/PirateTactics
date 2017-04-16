4415cc71c30ab19cf0f34584d63af8605ae23f9a
hankyonsoo <601107w@gmail.com>
2017-04-16 21:28:43 +0900

    .

---
+++ main.js
@@ -0,0 +1,25 @@
+/**
+ * enchant.js を使う前にフレームワークを有効するための必要な処理。
+ */
+enchant();
+
+/*
+ * ページがロードされた際に実行される関数。
+ * すべての処理はページがロードされてから行うため、 window.onload の中で実行する。
+ * 特に new Core(); は、<body> タグが存在しないとエラーになるので注意。
+ */
+window.onload = function(){
+    /**
+     * Core オブジェクトを作成する。
+     * 画面の大きさは 960ピクセル x 640ピクセル に設定する。
+     */
+    var game = new Core(960, 640); //この大きさだとモバイル端末でも遊べます
+
+    /**
+     * ゲームにシーンを追加
+     */
+    var sceneGameMain = new Scene();
+    game.pushScene(sceneGameMain);
+
+    game.start();
+};
